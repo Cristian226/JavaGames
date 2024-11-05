73a1db1c0c08b8285321b091f9fbb185a3e531c9
Cristian226 <cristi04pcc@gmail.com>
2024-11-05 15:34:31 +0200

    Main Menu update

--- src/main/java/Interfaces/MainMenu.java
+++ src/main/java/Interfaces/MainMenu.java
@@ -22,12 +22,37 @@ public class MainMenu  extends JFrame {
         AddButton("MineSweeper", "src/main/resources/controller.png");
         AddButton("X", "src/main/resources/controller.png");
 
+        JLabel mainMenuLabel = new JLabel();
+        mainMenuLabel.setText("MAIN MENU");
+        mainMenuLabel.setFont(new Font("Viner Hand ITC", Font.PLAIN, 40));
+        mainMenuLabel.setBounds(100,80,300,70);
+        mainMenuLabel.setHorizontalTextPosition(SwingConstants.CENTER);
+        mainMenuLabel.setVisible(true);
+        this.add(mainMenuLabel);
+
+        JButton statsButton = new JButton("Stats");
+        statsButton.setFont(new Font("Serif", Font.BOLD, 16));
+        statsButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        statsButton.setBounds(10,10,100,35);
+        statsButton.setBackground(new Color(0x0A4E8C));
+        statsButton.setFocusable(false);
+        statsButton.setForeground(Color.BLACK);
+        this.add(statsButton);
+
+        JButton changeUser = new JButton("Log out");
+        changeUser.setFont(new Font("Serif", Font.BOLD, 16));
+        changeUser.setHorizontalTextPosition(SwingConstants.CENTER);
+        changeUser.setBounds(325,10,100,35);
+        changeUser.setBackground(new Color(0x0A4E8C));
+        changeUser.setFocusable(false);
+        changeUser.setForeground(Color.BLACK);
+        this.add(changeUser);
+
         buttonsPanel.setBounds(25,200,390,140);
         buttonsPanel.setBackground(new Color(123,50,250));
         buttonsPanel.setLayout(new GridLayout(2,2,40,15));
         buttonsPanel.setVisible(true);
         this.add(buttonsPanel);
-
 
     }
 
--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,4 +10,5 @@ public class App
         MainMenu mainMenu = new MainMenu();
 
     }
+
 }
